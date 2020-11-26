7931ac9b115b213230fe7adc8551c8c275b9a473
Beppo <beppo2488sm@gmail.com>
2020-11-25 23:06:18 -0600

    [add] PhotoCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './GlobalStyles'
+import { ListOfPhotoCards } from './components/PhotoCard'
+
 
 export const App = () => (
     <>
         <GlobalStyles />
         <ListOfCategories />
+        <ListOfPhotoCards />
     </>
 )

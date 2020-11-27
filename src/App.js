d36832e64d1796c3f8eea49bcc8d8e9b726b7a26
Beppo <beppo2488sm@gmail.com>
2020-11-27 14:07:58 -0600

    [add] Refactoring

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './container/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Logo } from './components/Logo'
 
-export const App = () => (
-    <>
-        <GlobalStyles />
-        <Logo />
-        <ListOfCategories />
-        <ListOfPhotoCards categoryId={2}/>
-    </>
-)
+export const App = () => {
+    const urlParams = new window.URLSearchParams(window.location.search)
+    const detailId = urlParams.get('detail')
+    return (
+        <>
+            <GlobalStyles />
+            <Logo />
+            {
+                detailId
+                    ? <PhotoCardWithQuery id={detailId }/>
+                    : <>
+                        <ListOfCategories />
+                        <ListOfPhotoCards categoryId={1}/>
+                    </>
+            }
+            
+        </>
+    )
+}

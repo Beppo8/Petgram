07ac072a811b35b637fd6e22be8a5ed943912133
Beppo <beppo2488sm@gmail.com>
2020-11-28 01:03:57 -0600

    [add] Reach/Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './styles/GlobalStyles'
-import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Logo } from './components/Logo'
+import { Home } from './pages/Home'
+
+import { Router } from '@reach/router'
 
 export const App = () => {
     const urlParams = new window.URLSearchParams(window.location.search)
@@ -15,10 +16,10 @@ export const App = () => {
             {
                 detailId
                     ? <PhotoCardWithQuery id={detailId }/>
-                    : <>
-                        <ListOfCategories />
-                        <ListOfPhotoCards categoryId={1}/>
-                    </>
+                    : <Router>
+                        <Home path='/' />
+                        <Home path='/pet/:id'/>
+                    </Router>
             }
             
         </>
